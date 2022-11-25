f29102f616c50339a8bce31fbae998a6a5ee24e4
Audyssee <audrey.gentili38210@gmail.com>
2022-11-25 10:05:31 +0100

    td5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  nbItems = 4;
+  btnText = new String("Ajouter un élément");
+  objectifText = new String("");
+  objectifs = new Array();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { 
+    //this.monParam = route.snapshot.params['id'];
+  }
 
   ngOnInit(): void {
+    this.nbItems = this.objectifs.length;
+  }
+
+  ajoutItem(elt: String) {
+    this.objectifs.push(elt);
+    console.log(this.objectifs);
+    this.nbItems++;
   }
 
 }
